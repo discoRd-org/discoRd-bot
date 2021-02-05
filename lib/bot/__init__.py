de2874c27ae9b58e4a7964ef8aa74f0c0f4aeb8f
Alexander Lam <alexandercy.lam@gmail.com>
2021-02-05 11:29:44 -0500

    Added comments

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -1,10 +1,13 @@
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.schedulers.asyncio import AsyncIOScheduler 
 from discord.ext.commands import Bot as BotBase
-from discord import Intents
+# Discord's preivileged gateway intents 
+# (go to discord's Developer Portal > Application > discoRd > Bot)
+from discord import Intents 
 from discord import File
 from datetime import datetime
 from lib.bot.create_embed import create_embed
 
+# Bot command prefix
 PREFIX = "+"
 
 class Bot(BotBase):
@@ -14,11 +17,13 @@ class Bot(BotBase):
 		self.guild = None
 		self.scheduler = AsyncIOScheduler()
 
+		# Inherits BotBase
 		super().__init__(
 			command_prefix=PREFIX, 
 			intents=Intents.all()
 		)
 
+	# Initializes bot using TOKEN
 	def run(self, version):
 		self.VERSION = version
 
@@ -28,7 +33,6 @@ class Bot(BotBase):
 		print("running bot...")
 		super().run(self.TOKEN, reconnect=True)
 		
-
 	async def on_connect(self):
 		print("bot connected!")
 
@@ -38,12 +42,17 @@ class Bot(BotBase):
 	async def on_ready(self):
 		if not self.ready:
 			self.ready = True
+
+			# Set server-specific bot using server ID
+			# Can leave this out for multi-server bot
 			self.guild = self.get_guild(806626416783130674)
 			print("bot ready")
 
+			# Set channel using channel ID
 			channel = self.get_channel(806950823396769883)
 			await channel.send("Now online!")
 
+			# Create and send embed to channel
 			fields = [
 				("Name1", "Value1", True),
 				("Name2", "Value2", True),
@@ -68,7 +77,9 @@ class Bot(BotBase):
 		else:
 			print("bot reconnected")
 
+	# When bot receives a message from a channel
 	async def on_message(self, message):
 		pass
 
+# Create an instance of Bot
 bot = Bot()
--- lib/bot/create_embed.py
+++ lib/bot/create_embed.py
@@ -12,6 +12,23 @@ def create_embed(
 	image = None,
 	footer = None
 ):
+	"""Create an Embed
+
+	Args:
+		title (str): Set title
+		description (str): Set description
+		fields (tuple): Set fields
+		colour (int, optional): Set color. Defaults to None.
+		timestamp (datetime, optional): Set timestamp. Defaults to None.
+		author (str, optional): Set author. Defaults to None.
+		author_icon (str, optional): Set author icon using image url. Defaults to None.
+		thumbnail (str, optional): Set thumbnail using image url. Defaults to None.
+		image (str, optional): Set image using image url. Defaults to None.
+		footer (str, optional): Set footer. Defaults to None.
+
+	Returns:
+		embed: returns an embed
+	"""
 
 	embed = Embed(
 		title=title, 

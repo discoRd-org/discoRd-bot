e8e7d3750f5adc92ba837c4d83df0935c8eee494
eshom <erez@mandor>
2021-02-16 17:38:48 +0200

    Also move attachmentes
    But it isn't really working right now.

--- lib/cogs/commands.py
+++ lib/cogs/commands.py
@@ -1,5 +1,6 @@
-from discord import Member, TextChannel
+from discord import Member, TextChannel, File
 from discord.ext.commands import Cog, command
+import tempfile
 import lib.constants as const
 
 
@@ -16,7 +17,19 @@ class GeneralCommand(Cog):
 
     @command(name="move")
     async def move_message(self, ctx, member: Member,
-                           target_channel: TextChannel, num_msg: int = 1):
+                           target_channel: TextChannel, num_msg: int = 1,
+                           copy: bool = False):
+        """
+        Copy/Move message(s), mentioning the author. Several messages will
+        be consolidated to one message separated by a new line.
+
+        member: Member. Mention or user ID to move messages for.
+        target_channel: TextChannel. The channel to copy/move to.
+        num_msg(optional): Integer. How many user messages to move?
+                           Defaults to 1.
+        copy(optional): Bool. If True, the original messages will be deleted.
+                        Defaults to 'False'.
+        """
         print("Running 'move' command")
 
         # Delete calling message, and also avoids moving the command itself
@@ -33,8 +46,25 @@ class GeneralCommand(Cog):
                 counter += 1
 
         msg_to_move.reverse()  # Order messages collected from oldest to newest
+        msg_all = ""
+        attachments_all = list()
         for msg in msg_to_move:
-            await target_channel.send(msg.content)
+            attachments_all.extend(msg.attachments)
+            msg_all += msg.content + "\n"
+
+        await target_channel.send(f"Original message by: {member.mention}")
+        await target_channel.send(msg_all)
+
+        if attachments_all:
+            for att in attachments_all:
+                tmp = tempfile.TemporaryFile("w+b")
+                tmp.write(await att.read())
+                await target_channel.send(File(tmp, att.filename))
+                tmp.close()
+
+        if not copy:
+            for msg in msg_to_move:
+                await msg.delete()
 
     @Cog.listener()
     async def on_ready(self):
